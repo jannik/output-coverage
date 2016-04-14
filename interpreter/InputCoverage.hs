f32b19efd9229e6324d2f3fa84ae5b604f33ceb4
jannik <jannikgram@ishoejby.dk>
2016-04-14 18:13:21 +0200

    Switch to more monadic style in input coverage checker.

--- interpreter/InputCoverage.hs
+++ interpreter/InputCoverage.hs
@@ -1,11 +1,25 @@
 module InputCoverage where
 
 import Control.Monad (guard)
+
+import Control.Monad.Except (ExceptT, runExceptT, throwError)
+import Control.Monad.Identity (Identity, runIdentity)
+import Control.Monad.Reader (ReaderT, runReaderT, asks)
+import Control.Monad.State (StateT, evalStateT, get, modify)
+
 import Data.List (find)
 import Data.Maybe (isJust)
 
 import AST
 import Unify (unify)
+
+type InputCoverageError = String
+type Environment = [Type]
+type Context = Int
+type M = ExceptT InputCoverageError (StateT Context (ReaderT Environment Identity))
+
+runM :: M a -> Environment -> Context -> Either InputCoverageError a
+runM m env ctx = runIdentity . flip runReaderT env . flip evalStateT ctx . runExceptT $ m
 
 maxDepth :: Mode -> [Clause] -> Int
 maxDepth mo cls = maximum (0 : map maxDepth' cls)
@@ -13,32 +27,35 @@ maxDepth mo cls = maximum (0 : map maxDepth' cls)
     maxDepth' :: Clause -> Int
     maxDepth' (Clause _ (Struct _ tms) _) = maximum $ 0 : [ termDepth tm | (In, tm) <- zip mo tms ]
 
-findSplittingVariable :: Int -> [Term] -> [(ConName, TypeName, Int)] -> Maybe (ConName, TypeName, Int)
-findSplittingVariable maxDpth [] svars = Nothing
+findSplittingVariable :: Int -> [Term] -> [(ConName, TypeName, Int)] -> M (ConName, TypeName, Int)
+findSplittingVariable maxDpth [] svars = throwError "no splitting variables remain"
 findSplittingVariable maxDpth ((Var _) : tms) svars = findSplittingVariable maxDpth tms svars
 findSplittingVariable maxDpth ((Comp cnam []) : tms) svars =
   case find (\(cnam', _, _) -> cnam' == cnam) svars of
     Nothing -> findSplittingVariable maxDpth tms svars
     Just (svar@(_, _, currDpth))
-      | currDpth < maxDpth -> Just svar
+      | currDpth < maxDpth -> return svar
       | otherwise -> findSplittingVariable maxDpth tms svars
 findSplittingVariable maxDpth ((Comp cnam tms') : tms) svars = findSplittingVariable maxDpth (tms' ++ tms) svars
 
-genTerms :: Int -> Int -> [Constructor] -> (Int, [(ConName, TypeName, Int)], [Term])
-genTerms i _ [] = (i, [], [])
-genTerms i currDepth (cn : cns) =
-  let (i', svars, tm) = genTerm i currDepth cn
-      (i'', svars', tms) = genTerms i' currDepth cns
-  in (i'', svars ++ svars', tm : tms)
+genTerms :: Int -> [Constructor] -> M ([(ConName, TypeName, Int)], [Term])
+genTerms _ [] = return ([], [])
+genTerms currDepth (cn : cns) = do
+  (svars, tm) <- genTerm currDepth cn
+  (svars', tms) <- genTerms currDepth cns
+  return (svars ++ svars', tm : tms)
 
-genTerm :: Int -> Int -> Constructor -> (Int, [(ConName, TypeName, Int)], Term)
-genTerm i currDepth (Con cnam tpSig) =
-  let (i', cnams) = makeSplittingVariables i (length tpSig) []
-  in (i', zip3 cnams tpSig (repeat (currDepth + 1)), Comp cnam (map (\cnam' -> Comp cnam' []) cnams))
+genTerm :: Int -> Constructor -> M ([(ConName, TypeName, Int)], Term)
+genTerm currDepth (Con cnam tpSig) = do
+  cnams <- makeSplittingVariables (length tpSig) []
+  return (zip3 cnams tpSig (repeat (currDepth + 1)), Comp cnam (map (\cnam' -> Comp cnam' []) cnams))
 
-makeSplittingVariables :: Int -> Int -> [ConName] -> (Int, [ConName])
-makeSplittingVariables i 0 acc = (i, acc)
-makeSplittingVariables i len acc = makeSplittingVariables (i + 1) (len - 1) (acc ++ ["_" ++ show i])
+makeSplittingVariables :: Int -> [ConName] -> M [ConName]
+makeSplittingVariables 0 acc = return acc
+makeSplittingVariables len acc = do
+  cnam <- get >>= return . ("_" ++) . show
+  modify (+ 1)
+  makeSplittingVariables (len - 1) (acc ++ [cnam])
 
 instSplittingVariable :: ConName -> [Term] -> Term -> [Term]
 instSplittingVariable cnam tms tm = map instSV tms
@@ -48,45 +65,54 @@ instSplittingVariable cnam tms tm = map instSV tms
     instSV (Comp cnam' []) | cnam' == cnam = tm
     instSV (Comp cnam' tms') = Comp cnam' $ map instSV tms'
 
--- first (and last) int is fresh number supply, second int is max split depth
--- conname ints are current split depths; switch to monadic approach
-split :: Int -> Int -> [Type] -> [(ConName, TypeName, Int)] -> Structure -> [Structure] -> Maybe (Int, [(ConName, TypeName, Int)], [Structure])
-split i maxDpth tps svars (Struct pnam tms) strs = do
+-- first int is max split depth, conname ints are current split depths
+split :: Int -> [(ConName, TypeName, Int)] -> Structure -> [Structure] -> M ([(ConName, TypeName, Int)], [Structure])
+split maxDpth svars (Struct pnam tms) strs = do
   (cnam, tnam, currDpth) <- findSplittingVariable maxDpth tms svars
-  guard $ currDpth < maxDpth
-  (Typ _ cns) <- find (\(Typ tnam' cns) -> tnam' == tnam) tps -- should not fail in a well-typed program
-  let (i', svars', tms') = genTerms i currDpth cns
-      strs' = map ((Struct pnam) . (instSplittingVariable cnam tms)) tms'
+  guard $ currDpth < maxDpth -- define your own guard that carries a message
+  tp <- asks $ find (\(Typ tnam' cns) -> tnam' == tnam)
+  guard $ isJust tp -- should not fail in a well-typed program
+  let Just (Typ _ cns) = tp -- unsafe
+  (svars', tms') <- genTerms currDpth cns
+  let strs' = map ((Struct pnam) . (instSplittingVariable cnam tms)) tms'
       strs'' = concatMap (\(Struct pnam' tms'') -> map ((Struct pnam') . (instSplittingVariable cnam tms'')) tms') strs
-  Just (i', svars ++ svars', strs' ++ strs'')
+  return (svars ++ svars', strs' ++ strs'')
 
--- todo: add proper error handling
+-- temp
 inputCoverageCheck :: [Type] -> Program -> Bool
-inputCoverageCheck tps prog = all checkPredicate prog
+inputCoverageCheck tps prog =
+  case runM (inputCoverageCheck' prog) tps 0 of
+    Left _ -> False
+    Right _ -> True
+
+inputCoverageCheck' :: Program -> M ()
+inputCoverageCheck' prog = mapM_ checkPredicate prog
   where
-    checkPredicate :: Predicate -> Bool
-    checkPredicate (Pred pnam tpSig mo cls) =
+    checkPredicate :: Predicate -> M ()
+    checkPredicate (Pred pnam tpSig mo cls) = do
       let maxDpth = 1 + maxDepth mo cls
           heads = map (\(Clause _ str _) -> str) cls
-          (i, svars, tms) = initTerms 0 $ zip mo tpSig
-          str = (Struct pnam tms)
-      in check i maxDpth tps svars heads [str]
+      (svars, tms) <- initTerms $ zip mo tpSig
+      check maxDpth svars heads [Struct pnam tms]
 
-initTerms :: Int -> [(Polarity, TypeName)] -> (Int, [(ConName, TypeName, Int)], [Term])
-initTerms i [] = (i, [], [])
-initTerms i ((In, tnam) : args) =
-  let (i', svars, tms) = initTerms (i + 1) args
-      cnam = "_" ++ show i
-  in (i', (cnam, tnam, 0) : svars, Comp cnam [] : tms)
-initTerms i (_ : args) =
-  let (i', svars, tms) = initTerms (i + 1) args
-  in (i', svars, Var ("_", i) : tms)
+initTerms :: [(Polarity, TypeName)] -> M ([(ConName, TypeName, Int)], [Term])
+initTerms [] = return ([], [])
+initTerms ((In, tnam) : args) = do
+  cnam <- get >>= return . ("_" ++) . show
+  modify (+ 1)
+  (svars, tms) <- initTerms args
+  return $ ((cnam, tnam, 0) : svars, Comp cnam [] : tms)
+initTerms (_ : args) = do
+  ident <- get >>= return . (,) "_"
+  modify (+ 1)
+  (svars, tms) <- initTerms args
+  return (svars, Var ident : tms)
 
-check :: Int -> Int -> [Type] -> [(ConName, TypeName, Int)] -> [Structure] -> [Structure] -> Bool
-check _ _ _ _ _ [] = True
-check i maxDpth tps svars heads (str : strs) =
+check :: Int -> [(ConName, TypeName, Int)] -> [Structure] -> [Structure] -> M ()
+check _ _ _ [] = return ()
+check maxDpth svars heads (str : strs) =
   if any (\str' -> isJust (unify (structureToTerm str') (structureToTerm str))) heads
-    then check i maxDpth tps svars heads strs
-    else case split i maxDpth tps svars str strs of
-      Nothing -> False
-      Just (i', svars', strs') -> check i' maxDpth tps svars' heads strs'
+    then check maxDpth svars heads strs
+    else do
+      (svars', strs') <- split maxDpth svars str strs
+      check maxDpth svars' heads strs'
